0a4493312e3a6352099c2a3b881282ebf23113a0
Tom Warner <tomwamt@gmail.com>
2021-11-30 22:09:24 -0700

    Day 1

--- src/day1.rs
+++ src/day1.rs
@@ -4,10 +4,13 @@ pub struct Solution;
 
 impl crate::Solution for Solution {
    fn solve_1(&self, input: String) -> String {
-       input
+       let depths: Vec<i32> = input.lines().map(|line| line.parse().unwrap()).collect();
+       depths.windows(2).filter(|it| it[0] < it[1]).count().to_string()
    }
 
    fn solve_2(&self, input: String) -> String {
-       todo!()
+       let depths: Vec<i32> = input.lines().map(|line| line.parse().unwrap()).collect();
+       let windows: Vec<i32> = depths.windows(3).map(|it| it.iter().sum()).collect();
+       windows.windows(2).filter(|it| it[0] < it[1]).count().to_string()
    }
 }
