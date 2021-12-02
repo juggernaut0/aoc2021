16a1d3f536e70512ac1ffcae61d77b119a1c7269
Tom Warner <tomwamt@gmail.com>
2021-12-01 22:18:22 -0700

    Day 2

--- src/day2.rs
+++ src/day2.rs
@@ -1,13 +1,54 @@
 #![allow(unused_variables)]
 
+use std::str::FromStr;
+
 pub struct Solution;
 
 impl crate::Solution for Solution {
    fn solve_1(&self, input: String) -> String {
-       todo!()
+       let (x, y) = input
+           .lines()
+           .map(|it| it.parse::<Direction>().unwrap())
+           .fold((0, 0), |(x, y), b| {
+               match b {
+                   Direction::Forward(n) => (x + n, y),
+                   Direction::Down(n) => (x, y + n),
+               }
+           });
+       (x * y).to_string()
    }
 
    fn solve_2(&self, input: String) -> String {
-       todo!()
+       let (x, y, _) = input
+           .lines()
+           .map(|it| it.parse::<Direction>().unwrap())
+           .fold((0, 0, 0), |(pos, depth, aim), b| {
+               match b {
+                   Direction::Forward(n) => (pos + n, depth + aim*n, aim),
+                   Direction::Down(n) => (pos, depth, aim + n),
+               }
+           });
+       (x * y).to_string()
    }
+}
+
+enum Direction {
+    Forward(i32),
+    Down(i32),
+}
+
+impl FromStr for Direction {
+    type Err = String;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let mut parts = s.split_whitespace();
+        let dir = parts.next().ok_or_else(|| s.to_string())?;
+        let n: i32 = parts.next().ok_or_else(|| s.to_string())?.parse().map_err(|_| s.to_string())?;
+        match dir {
+            "forward" => Ok(Direction::Forward(n)),
+            "down" => Ok(Direction::Down(n)),
+            "up" => Ok(Direction::Down(-n)),
+            _ => Err(s.to_string())
+        }
+    }
 }
