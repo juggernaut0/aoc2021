4235164576b252bf75bb3af964a63808c63f3af5
Tom Warner <tomwamt@gmail.com>
2021-12-02 23:47:58 -0700

    Day 3

--- src/day3.rs
+++ src/day3.rs
@@ -1,13 +1,113 @@
-#![allow(unused_variables)]
+use std::cmp::Ordering;
+use crate::util::parse_lines_with;
 
 pub struct Solution;
 
 impl crate::Solution for Solution {
    fn solve_1(&self, input: String) -> String {
-       todo!()
+       let lines: Vec<BitStr> = parse_lines_with(&input, |s| BitStr::new(s).unwrap()).collect();
+       let last = lines[0].len();
+
+       let mut res = String::new();
+       for i in 0..last {
+           if most_common(&lines, i).unwrap() {
+               res.push('1')
+           } else {
+               res.push('0')
+           }
+       }
+       let g = BitStr::new(&res).unwrap().to_u32();
+       let e = !g & !(u32::MAX << last);
+       log::info!("g = {} e = {}", g, e);
+       (e * g).to_string()
    }
 
    fn solve_2(&self, input: String) -> String {
-       todo!()
+       let lines: Vec<BitStr> = parse_lines_with(&input, |s| BitStr::new(s).unwrap()).collect();
+       let last = lines[0].len();
+
+       let a = {
+           let mut remaining: Vec<_> = lines.clone();
+           for i in 0..last {
+               if remaining.len() == 1 { break; }
+               let cond = most_common(&remaining, i).unwrap_or(true);
+               remaining.retain(|l| l.get(i) == cond);
+           }
+           remaining[0].to_u32()
+       };
+       log::info!("{}", a);
+
+       let b = {
+           let mut remaining: Vec<_> = lines;
+           for i in 0..last {
+               if remaining.len() == 1 { break; }
+               let cond = !most_common(&remaining, i).unwrap_or(true);
+               remaining.retain(|l| l.get(i) == cond);
+           }
+           remaining[0].to_u32()
+       };
+       log::info!("{}", b);
+
+       (a*b).to_string()
    }
+}
+
+#[derive(Copy, Clone)]
+struct BitStr<'a>(&'a str);
+
+impl BitStr<'_> {
+    fn new(s: &str) -> Option<BitStr> {
+        if s.chars().all(|c| c == '0' || c == '1') {
+            Some(BitStr(s))
+        } else {
+            None
+        }
+    }
+
+    fn len(&self) -> usize {
+        self.0.len()
+    }
+
+    fn to_u32(self) -> u32 {
+        u32::from_str_radix(self.0, 2).unwrap()
+    }
+
+    fn get(&self, index: usize) -> bool {
+        self.0.chars().nth(index).unwrap() == '1'
+    }
+}
+
+fn most_common(v: &[BitStr], i: usize) -> Option<bool> {
+    let count1 = v.iter().filter(|l| l.get(i)).count();
+    match (count1*2).cmp(&v.len()) {
+        Ordering::Less => Some(false),
+        Ordering::Equal => None,
+        Ordering::Greater => Some(true),
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use log::Level;
+    use crate::Solution;
+    use super::*;
+    #[test]
+    fn test_2() {
+        simple_logger::init_with_level(Level::Trace).unwrap();
+        let inp = "\
+00100
+11110
+10110
+10111
+10101
+01111
+00111
+11100
+10000
+11001
+00010
+01010
+".to_string();
+        assert_eq!(&Solution.solve_2(inp), "230");
+    }
 }
--- src/util.rs
+++ src/util.rs
@@ -1,9 +1,13 @@
 use std::fmt::Debug;
 use std::str::FromStr;
 
-pub fn parse_lines<T: FromStr>(input: &str) -> impl Iterator<Item=T> + '_
+pub fn parse_lines<'a, T: FromStr + 'a>(input: &'a str) -> impl Iterator<Item=T> + 'a
     where T::Err: Debug
 {
+    parse_lines_with(input, |line| line.parse().unwrap())
+}
+
+pub fn parse_lines_with<'a, T: 'a, P: FnMut(&'a str) -> T + 'a>(input: &'a str, parser: P) -> impl Iterator<Item=T> + 'a {
     input.lines()
-        .map(|line| line.parse().unwrap())
+        .map(parser)
 }
